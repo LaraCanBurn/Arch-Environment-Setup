607b06f050d14a278e584638d74e1884850aa807
Alvaro <alade2240@gmail.com>
2025-03-25 20:07:52 +0100

    Second commit - Documentation Update & Scripts

---
+++ Installation/scripts/ArchInstall_Encryption.sh
@@ -0,0 +1,50 @@
+# Initial SetUp
+    #
+    #
+    #
+
+# Create Partitions
+    #
+    #
+
+# Luks configuration & LVM
+    #
+    #
+    #
+    #
+
+# Raid configuration ZFS
+    #
+    #
+    #
+    #
+
+# Mount Partitions
+    #
+    #
+    #
+    # 
+
+# Install Arch
+    #
+    #
+
+# System configuration
+    #
+    #
+    #
+
+# Post Installation
+    #
+    #
+    #
+
+# Install Graphic Environment
+    #
+    #
+    #
+
+# Final Configuration & Security
+    #
+    #
+    #
